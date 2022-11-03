c6d34761f6972d4b5a089d35e0f95514f5aad1be
lismgmk <lismgmk2@gmail.com>
2022-11-03 17:54:44 +0300

    test-2

--- src/modules/blogs/blogs.service.ts
+++ src/modules/blogs/blogs.service.ts
@@ -1,6 +1,6 @@
-// import { INCORRECT_TYPE_VALIDATION_ERROR } from './../../consts/ad-validation-const';
+import { INCORRECT_TYPE_VALIDATION_ERROR } from './../../consts/ad-validation-const';
 import { Injectable } from '@nestjs/common';
-import { INCORRECT_TYPE_VALIDATION_ERROR } from '@app/consts/ad-validation-const';
+// import { INCORRECT_TYPE_VALIDATION_ERROR } from '@app/consts/ad-validation-const';
 
 @Injectable()
 export class BlogsService {
