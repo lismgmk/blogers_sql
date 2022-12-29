6f01e65f2fb3e199b289cad832efbdfee2d077d9
lismgmk <lismgmk2@gmail.com>
2022-12-29 14:42:26 +0300

    add some docker fix-2

--- create_docker_db.sh
+++ create_docker_db.sh
@@ -3,12 +3,4 @@
 # Скрипт запускается для разворачивания докера и базы.
 
 cd ./deploy/dev && \
-docker-compose -p pg-server build && \
-docker-compose -p pg-server up -d postgres && \
-# Ожидаем пока база завершит подготовку к работе (Если не работает, заменить на sleep)
-./wait-for-it.sh -p postgres:5432 -- node index.js && \
-# sleep 5 
-echo DBPassword | docker-compose -p pg-server run --no-deps postgres psql -h postgres -U postgres -c "CREATE DATABASE blog_sql-db;"
-echo 'npm run typeorm:run-migration' | docker-compose -p pg-server run server sh 
-docker-compose -p pg-server down
-# docker-compose -p todolist-graphql-server up
+sudo  docker run --name 14.6vova --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=2156128 -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp:/var/lib/postgresql/data -p 5432:5432  postgres:14.6
